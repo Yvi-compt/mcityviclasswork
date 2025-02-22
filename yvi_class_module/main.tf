32405365918fe3b108a43afdc031637f54335a82
Yvi-compt <yvesplacide@gmail.com>
2025-02-21 20:15:33 -0500

    Update main.tf

--- yvi_class_module/main.tf
+++ yvi_class_module/main.tf
@@ -10,7 +10,7 @@ module "linuxservers" {
     source              = "Azure/vm/azurerm"
     location            = "eastus"
     vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
+    admin_password      = var.admin_password
     vm_os_simple        = "WindowsServer"
     public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
     vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"

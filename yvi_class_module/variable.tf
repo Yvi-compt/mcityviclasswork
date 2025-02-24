2e8f467d3283465ad00e3e4e3f37bf17ba308dac
Yvi-compt <yvesplacide@gmail.com>
2025-02-24 17:24:42 -0500

    Update variable.tf

--- yvi_class_module/variable.tf
+++ yvi_class_module/variable.tf
@@ -4,6 +4,8 @@ variable "resource_group_name" {}
 variable "subnet_id" {}
 variable "vm_size" {}
 variable "admin_username" {}
-variable "admin_password" {}
+variable "admin_password" {
+type = string
+}
 variable "is_windows" { type = bool }
 variable "ssh_key_path" { default = "~/.ssh/id_rsa.pub" }

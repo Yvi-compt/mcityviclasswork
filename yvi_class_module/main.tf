26aa38f42390506011d5a1c804820080d179d3bb
Yvi-compt <yvesplacide@gmail.com>
2025-02-24 17:00:39 -0500

    Update main.tf

--- yvi_class_module/main.tf
+++ yvi_class_module/main.tf
@@ -1,24 +1,47 @@
-astmodule "linuxservers" {
-    source              = "Azure/vm/azurerm"
-    location            = "eastus"
-    vm_os_simple        = "UbuntuServer"
-    public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
+resource "azurerm_linux_virtual_machine" "linux_yvivm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [var.network_interface_id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
- 
-  module "windowsservers" {
-    source              = "Azure/vm/azurerm"
-    location            = "eastus"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = var.admin_password
-    vm_os_simple        = "WindowsServer"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_key_path)
   }
- 
-  module "vnet" {
-    source              = "Azure/vnet/azurerm"
-    version             = "~> 1.0.0"
-    location            = "eastus"
-    resource_group_name = "terraform-vm"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
   }
+}
+resource "azurerm_windows_virtual_machine" "windows_yvivm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [var.network_interface_id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter-smalldisk"
+    version   = "latest"
+  }
+}
+
